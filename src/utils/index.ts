17674b98a451f341f91aabcbdf07aa02e2faec30
Ross Sullivan <rosssullivan101@gmail.com>
2026-05-10 20:06:56 +0900

    bug fixes for the format relative time

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,26 +3,26 @@ export function formatRelativeTime(isoDate: string): string {
 	const diffMin = Math.floor(diffMs / 60000);
 	if (diffMin === 0) return "now";
 	if (diffMin < 60)
-		return diffMin === 1 ? `${diffMin} mins ago` : `${diffMin} min ago`;
+		return diffMin === 1 ? `1 min ago` : `${diffMin} mins ago`;
 
 	const diffHr = Math.floor(diffMin / 60);
 	if (diffHr < 24)
-		return diffHr === 1 ? `${diffHr} hour ago` : `${diffHr} hours ago`;
+		return diffHr === 1 ? `1 hour ago` : `${diffHr} hours ago`;
 
 	const diffDay = Math.floor(diffHr / 24);
 	if (diffDay < 7)
-		return diffDay === 1 ? `${diffDay} day ago` : `${diffDay} days ago`;
+		return diffDay === 1 ? `1 day ago` : `${diffDay} days ago`;
 
 	const diffWeek = Math.floor(diffDay / 7);
 	if (diffWeek < 4)
-		return diffWeek === 1 ? `${diffWeek} week ago` : `${diffWeek} weeks ago`;
+		return diffWeek === 1 ? `1 week ago` : `${diffWeek} weeks ago`;
 
-	const diffMonth = Math.floor(diffDay / 30.44);
+	const diffMonth = Math.floor(diffDay / 30);
 	if (diffMonth < 12)
 		return diffMonth === 1
-			? `${diffMonth} month ago`
+			? `1 month ago`
 			: `${diffMonth} months ago`;
 
-	const diffYear = Math.floor(diffDay / 365.25);
-	return diffYear === 1 ? `${diffYear} year ago` : `${diffYear} years ago`;
+	const diffYear = Math.floor(diffDay / 365);
+	return diffYear === 1 ? `1 year ago` : `${diffYear} years ago`;
 }
