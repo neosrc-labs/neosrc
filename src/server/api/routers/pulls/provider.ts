a0051f09953f0a51460d9a23cca04346b1cf15a2
Ross Sullivan <rosssullivan101@gmail.com>
2026-06-14 18:20:17 +0900

    Adding multi-provider interfaces

---
+++ src/server/api/routers/pulls/provider.ts
@@ -0,0 +1,13 @@
+import type { db } from "~/server/db";
+import type { PrSearchResult, SearchParams } from "./types";
+
+export interface PullRequestProvider {
+    search(params: SearchParams & { ctx: Ctx }): Promise<PrSearchResult>;
+}
+
+export type Ctx = {
+    db: typeof db;
+    session: { user: { id: string } };
+};
+
+export type ProviderType = "gh" | "cb";
