a5c32a7e168c5860a615fc0fa9d68251cc6e1b32
Ross Sullivan <rosssullivan101@gmail.com>
2026-09-15 19:27:10 +0900

    refactor(commits): hoist commits page to src/components/commit
    The 6 commit-list files move to src/components/commit/; the redirect route's
    commits/ directory existed only to host _components/, so it is removed.
    Consumers are the gh/cb commits/[branch] pages. Rewrites 4 specifiers.
    No behaviour change.

--- src/app/gh/[owner]/[repo]/commits/[branch]/page.tsx
+++ src/app/gh/[owner]/[repo]/commits/[branch]/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { CommitsList } from "~/app/[owner]/[repo]/commits/_components/commits-list";
-import { ghConfig } from "~/app/[owner]/[repo]/commits/_components/commits-list-config";
+import { CommitsList } from "~/components/commit/commits-list";
+import { ghConfig } from "~/components/commit/commits-list-config";
 
 export async function generateMetadata({
     params,
